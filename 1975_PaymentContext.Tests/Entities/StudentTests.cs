f354f4fbff4d903e122069e92e9a8dfb82f24ff1
Flavio Zavarise <flaviofz@gmail.com>
2020-10-09 17:10:11 -0300

    Criando classes de pagamentos, construtores e herança de pagamento

---
+++ 1975_PaymentContext.Tests/Entities/StudentTests.cs
@@ -0,0 +1,20 @@
+using _1975_PaymentContext.Domain.Entities;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace _1975_PaymentContext.Tests
+{
+    [TestClass]
+    public class StudentTests
+    {
+        [TestMethod]
+        public void AdicionarAssinatura()
+        {
+            var subscription = new Subscription(null);
+            var student = new Student("Flavio", "Zavarise", "13120696781", "flaviofz@gmail.com");
+
+            student.AddSubscription(subscription);
+
+            //var payment = new PayPalPayment();
+        }
+    }
+}
